c4e8425b6da143d8c06cd68d1c6ad5f16346d3e1
Kaiser Sajo <jhonkaisersajo@gmail.com>
2023-05-21 23:46:13 +0800

    input

---
+++ user_interface.py
@@ -0,0 +1,16 @@
+class User_Interface:
+    def ask_operation(self):
+        print("=" * 45)
+        print("Please choose an operation: \n"
+              "1. Addition\n"
+              "2. Subtraction\n"
+              "3. Multiplication\n"
+              "4. Division")
+        print("=" * 45)
+        
+    def input_operation(self):
+        operation = int(input("What operation would you like to use? (1-4): "))
+    
+    def calculator():
+        
+        
