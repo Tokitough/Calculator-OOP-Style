4364e42b9d82d1062da71d28c3429f6ba765d51d
Kaiser Sajo <jhonkaisersajo@gmail.com>
2023-05-21 23:45:28 +0800

    input

---
+++ calculator_oop_style.py
@@ -0,0 +1,9 @@
+from user_interface import User_Interface
+
+calculator()
+# Ask user to choose an operation
+# Get user input for operation
+# Get user input for two numbers
+# Perform operation based on user input
+# Display the result
+# Ask if the user wants to perform another calculation
